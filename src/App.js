6860ec2b648d82721e6842fda246db63be36f4ec
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-01-23 20:40:26 +0000

    get the list of transactions,current block and individual transactions from blockchain

--- src/App.js
+++ src/App.js
@@ -13,15 +13,61 @@ const alchemy = new Alchemy(settings);
 function App() {
 	const [blockNumber, setBlockNumber] = useState();
 
+	const [blockDetails, setBlockDetails] = useState();
+	const [blockWithTransactions, setBlockWithTransactions] = useState();
+
+	// useEffect: use getBlockNumber() from alchemy.core
 	useEffect(() => {
 		async function getBlockNumber() {
-			setBlockNumber(await alchemy.core.getBlockNumber());
+			// Call the alchemy.core.getBlockNumber method to retrieve the current block number from the Ethereum blockchain
+			const blockNumber = await alchemy.core.getBlockNumber();
+
+			// Update the component's state with the current block number
+			console.log("blockNumber: ", blockNumber);
+			setBlockNumber(blockNumber);
 		}
-
+		// Call the getBlockNumber function
 		getBlockNumber();
-	});
 
-	return <div className='App'>Block Number: {blockNumber}</div>;
+		async function getBlock() {
+			// Call the alchemy.core.getBlock method to retrieve the details of the blockNumber
+			const blockDetails = await alchemy.core.getBlock(blockNumber);
+
+			// Update the component's state with the current block number
+			console.log("blockDetails: ", blockDetails);
+			setBlockDetails(blockDetails.transactions);
+		}
+		// Call the getBlock function
+		getBlock();
+
+		async function getBlockWithTransactions() {
+			// Call the alchemy.core.getBlockWithTransactions method to retrieve the details of the blockNumber
+			const blockWithTransactions = await alchemy.core.getBlockWithTransactions(
+				blockNumber
+			);
+
+			// Update the component's state with the current block number
+			console.log("transaction details: ", blockWithTransactions.transactions);
+			setBlockWithTransactions(blockWithTransactions.transactions.length);
+		}
+		// Call the getBlockWithTransactions function
+		getBlockWithTransactions();
+	}, [blockNumber]); // The second argument to useEffect is an array of dependencies, which can be left empty in this case because the effect does not depend on any values from props or state
+
+	return (
+		<div className='blockexplorer-container'>
+			<h1>Block Explorer</h1>
+			<div className='App'>
+				<p>Block Number (getBlockNumber): {blockNumber}</p>
+				<p>Block Details (getBlock): {blockDetails}</p>
+				<p>
+					Block with Transactions (getBlockWithTransactions):{" "}
+					{blockWithTransactions}
+				</p>
+			</div>
+			;
+		</div>
+	);
 }
 
 export default App;
