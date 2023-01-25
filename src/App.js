29a130dec19f35a833ea065e9882113b13a92f77
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-01-25 20:26:59 +0000

    Return all the components into App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Alchemy, Network } from "alchemy-sdk";
+import Button from "react-bootstrap/Button";
+import Account from "./components/Account";
 import Header from "./components/Header";
-import Alert from "react-bootstrap/Alert";
-import Form from "react-bootstrap/Form";
+import Nav from "./components/Content";
+import "./App.css";
+import { Row, Col } from "react-bootstrap";
 
 const settings = {
 	apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
@@ -36,6 +39,34 @@ function App() {
 		<div className='App'>
 			<div className='container'>
 				<Header blockNumber={blockInfo} />
+				<Row>
+					<Col className='col-6'>
+						{!showBlock && (
+							<Button
+								style={{ margin: "15px" }}
+								variant='primary'
+								type='submit'
+								onClick={blockInfo}
+							>
+								Get Block Data
+							</Button>
+						)}
+						{showBlock && <Nav alchemy={alchemy} />}
+					</Col>
+					<Col className='col-6'>
+						{!showAccount && (
+							<Button
+								variant='primary'
+								type='submit'
+								style={{ margin: "15px" }}
+								onClick={accountInfo}
+							>
+								Show Account Information
+							</Button>
+						)}
+						{showAccount && <Account alchemy={alchemy} />}
+					</Col>
+				</Row>
 			</div>
 		</div>
 	);
