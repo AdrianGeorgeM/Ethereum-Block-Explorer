6c3828e9d2097fcd00ddadf161f6a48942945015
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-01-25 20:15:11 +0000

    Created Header Component for the current blocknumber

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = (props) => {
+	const { blockNumber } = props;
+
+	return (
+		<header className='card'>
+			<h2>Welcome to Adrian Block Explorer</h2>
+			<h4>Current: {blockNumber}</h4>
+		</header>
+	);
+};
+
+export default Header;
